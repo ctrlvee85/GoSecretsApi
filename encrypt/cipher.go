4a9f974d12ec8b20954caafff665e16eb938a471
Jen McCall <jmccall@gogoair.com>
2024-03-19 12:48:14 -0600

    Optional exercise code

--- encrypt/cipher.go
+++ encrypt/cipher.go
@@ -1,15 +1,65 @@
 package encrypt
 
 import (
-	"crypto/md5"
 	"encoding/hex"
+	"crypto/cipher"
+	"errors"
+	"crypto/md5"
+	"crypto/aes"
+	"fmt"
+	"io"
+	"crypto/rand"
 )
 
-func Encrypt(key []byte) {
+func Encrypt(key, plaintext string) (string, error) {
 
-	// key to encrypt
+	// key to encode secret string
+	block, err := newCipherBlock(key)
+	if err != nil {
+		return "", err
+	}
+	//read key
+	cipherText := make([]byte, aes.BlockSize+len(plaintext))
+	iv := cipherText[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader,iv); err != nil {
+		return "", err
+	}
+
+	// key destination
+	stream := cipher.NewCFBEncrypter(block,iv)
+	stream.XORKeyStream(cipherText[aes.BlockSize:], []byte(plaintext))
+
+	return fmt.Sprintf("%x", cipherText), nil
+
+}
+
+func Decrypt(key, cipherHex string) (string, error) {
+	block, err := newCipherBlock(key)
+	if err != nil {
+		return "", err
+	}
+
+	cipherText,err := hex.DecodeString(cipherHex)
+	if err != nil {
+		return "", err
+	}
+
+	if len(cipherText) < aes.BlockSize {
+		return "", errors.New("ciphertext too short")
+	}
+	iv := cipherText[:aes.BlockSize]
+	cipherText = cipherText[aes.BlockSize:]
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(cipherText,cipherText)
+
+
+	return string(cipherText), nil
+}
+
+func newCipherBlock(key string) (cipher.Block, error) {
 	hasher := md5.New()
-	hasher.Write([]byte(key))
-	key, _ := hex.DecodeString()
-
+	fmt.Fprint(hasher, key)
+	cipherKey := hasher.Sum(nil)
+	return aes.NewCipher(cipherKey)
 }
--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"crypto/aes"
+	"GoSecretsAPI/encrypt"
 	"crypto/md5"
-	"fmt"
 )
 
 func main() {
-	hasher := md5.New()
-	key := []byte("a bunch of words and 5484854 and stuff")
+	hasher = md5.New()
 	hasher.Write([]byte(key))
-	cipherKey := hasher.Sum(nil)
-	fmt.Println(len(cipherKey))
-	_, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
-	}
+	encrypt.Encrypt("dfghjk")
 }
